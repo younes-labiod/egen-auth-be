7ec7843cd80199e6aa0e4800cb09cfb49acc4620
younes-labiod <youneslabiod@gmail.com>
2024-12-07 20:43:45 +0400

    Add logging in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,87 +3,84 @@ import {
   BadRequestException,
   UnauthorizedException,
   OnModuleInit,
-} from '@nestjs/common';
-import { UserService } from '../users/user.service';
-import * as bcrypt from 'bcryptjs';
-import * as jwt from 'jsonwebtoken';
-import { SecretsManagerService } from 'src/config/secrets-manager.service';
+  Logger,
+} from "@nestjs/common";
+import { UserService } from "../users/user.service";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+import { SecretsManagerService } from "src/config/secrets-manager.service";
+import { RequestContext } from "src/utils/request-context";
 
 @Injectable()
 export class AuthService implements OnModuleInit {
+  private readonly logger = new Logger(AuthService.name);
   private jwtSecret: string;
 
   constructor(
     private readonly userService: UserService,
-    private readonly secretsManagerService: SecretsManagerService,
+    private readonly secretsManagerService: SecretsManagerService
   ) {}
 
   async onModuleInit() {
-    const secretName = process.env.JWT_SECRET_NAME || 'prod/jwt-secret';
+    const secretName = process.env.JWT_SECRET_NAME || "prod/jwt-secret";
     const secretString = await this.secretsManagerService.getSecret(secretName);
 
     if (secretString) {
       const secret = JSON.parse(secretString);
       this.jwtSecret = secret.JWT_SECRET;
     } else {
-      throw new Error('Failed to load JWT_SECRET from Secrets Manager.');
+      throw new Error("Failed to load JWT_SECRET from Secrets Manager.");
     }
   }
 
   async signIn(email: string, password: string) {
+    const transactionUuid = RequestContext.getTransactionUuid();
+    this.logger.log(`[${transactionUuid}] - Authenticating user ${email}`);
+
     const user = await this.userService.findByEmail(email);
     if (!user) {
-      throw new UnauthorizedException('Invalid credentials.');
+      this.logger.debug(`[${transactionUuid}] - Invalid credentials.`);
+      throw new UnauthorizedException(
+        `[${transactionUuid}] - Invalid credentials.`
+      );
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid credentials.');
+      this.logger.debug(`[${transactionUuid}] - Invalid credentials.`);
+      throw new UnauthorizedException(
+        `[${transactionUuid}] - Invalid credentials.`
+      );
     }
 
     if (!this.jwtSecret) {
-      throw new Error('JWT_SECRET is not initialized.');
+      this.logger.debug(
+        `[${transactionUuid}] - JWT_SECRET is not initialized.`
+      );
+      throw new Error(`[${transactionUuid}] - JWT_SECRET is not initialized.`);
     }
 
     const token = jwt.sign(
       { id: user._id, email: user.email },
       this.jwtSecret,
-      { expiresIn: '1h' },
+      { expiresIn: "1h" }
     );
     return { token };
   }
 
   async signUp(email: string, name: string, password: string) {
+    const transactionUuid = RequestContext.getTransactionUuid();
+    this.logger.log(`[${transactionUuid}] - Creating user ${email}`);
+
     const existingUser = await this.userService.findByEmail(email);
     if (existingUser) {
-      throw new BadRequestException('Email is already taken.');
+      this.logger.debug(`[${transactionUuid}] - Email is already taken.`);
+      throw new BadRequestException(
+        `[${transactionUuid}] - Email is already taken.`
+      );
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     return this.userService.create({ email, name, password: hashedPassword });
   }
-
-  /*async signInw(email: string, password: string) {
-    const user = await this.userService.findByEmail(email);
-    if (!user) {
-      throw new UnauthorizedException('Invalid credentials.');
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid credentials.');
-    }
-
-    // Retrieve secret from environment variables
-    const secret = this.configService.get<string>('JWT_SECRET');
-    if (!secret) {
-      throw new Error('JWT_SECRET is not defined in the environment.');
-    }
-
-    // Sign the JWT token
-    const token = jwt.sign({ id: user._id, email: user.email }, secret, {
-      expiresIn: '1h',
-    });
-    return { token };
-  }*/
 }
