2ca9e5722a4eb75fdc9b5915bbbf7de0e7756b33
younes-labiod <youneslabiod@gmail.com>
2024-12-08 16:37:09 +0400

    Add get profile API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,31 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { SignUpDto } from './dtos/sign-up.dto';
-import { SignInDto } from './dtos/sign-in.dto';
+import { Controller, Post, Body, Get, Request } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { SignUpDto } from "./dtos/sign-up.dto";
+import { SignInDto } from "./dtos/sign-in.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
+  @Post("signup")
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(
       signUpDto.email,
       signUpDto.name,
-      signUpDto.password,
+      signUpDto.password
     );
   }
 
-  @Post('signin')
+  @Post("signin")
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @Get("profile")
+  getProfile(@Request() req: any) {
+    return {
+      message: `Welcome to the application, ${req.user}`,
+      user: req.user,
+    };
   }
 }
