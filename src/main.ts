5c2069d35878b2d21873be0fd4d6dbe88c61b2e7
younes-labiod <youneslabiod@gmail.com>
2024-12-09 18:21:49 +0400

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FE_ORIGIN,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
